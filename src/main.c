424b7e31cd6306833dab705ed8b5b41d9173904a
Pedro Lopes <peperibeirolopes-bot>
2026-04-24 12:56:02 -0300

    adicionado printf das opcoes 2, 3 e 4 para calulcar media, verifysituacao e ver media

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@
 void exibirMenu() {
     printf("\n--- MENU ---\n");
     printf("1. Informar Notas\n");
+    printf("2. Calcular Media\n");
+    printf("3. Verificar Situacao\n");
+    printf("4. Ver Media Atual\n");
+
 
 int main() {
     
