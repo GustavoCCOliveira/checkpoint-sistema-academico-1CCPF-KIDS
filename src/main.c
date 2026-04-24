c00c6bcc0d0daba1025eb84d6c34a42cf66b6685
Rafael <rafalapregagm2@gmail.com>
2026-04-24 13:48:23 -0300

    fora adicionados ponto e virgula nas linhas 18 e 19 das respectivas funçoes.

--- src/main.c
+++ src/main.c
@@ -15,8 +15,8 @@ int main() {
     int opcao;
     float n1 = 0, n2 = 0, n3 = 0, n4 = 0, media = -1, notarec = 0;
     int a, b, c;
-    int coef_const
-    int coef_x
+    int coef_const;
+    int coef_x;
 do {
         exibirMenu();
         scanf("%d", &opcao);
