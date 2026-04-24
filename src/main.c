774fdb5aefb0fd1d706fc0bf2c64512f507909c4
lucasfurquimlima-hue <lucasfurquimlima@gmail.com>
2026-04-24 13:00:16 -0300

    foi adicionado int opção e os valores para n1, n2, n3, media, notarec epara declarar variavel da linha 14 até a linha 17.

--- src/main.c
+++ src/main.c
@@ -11,6 +11,10 @@ void exibirMenu() {
     printf("Escolha uma opcao: ");
 }
 
+int main() 
+    int opcao;
+    float n1 = 0, n2 = 0, n3 = 0, n4 = 0, media = -1, notarec = 0;
+    int a, b, c;
 
 int main() {
     
